0048af43bd8bcb27d118e7094e219767ff587baf
Mega-117 <enricosaian@hotmail.it>
2021-11-02 17:47:10 +0100

    completato

--- js/script.js
+++ js/script.js
@@ -11,20 +11,28 @@ window.addEventListener("DOMContentLoaded", function () {
 
         },
         methods: {
+            /* removeTask(indiceDaCancellare) {
+                this.taskList.splice(indiceDaCancellare, 1);
+            }, */
+
             addToTaskList() {
                 /* controllo doppioni */
                 let elementoEsiste = this.taskList.find((element) => {
                     return element.toLowerCase() === this.newTaskItem.toLowerCase();
                 });
+                if (this.newTaskItem === "") {
+                    return alert("errore: contenuto mancante");
+                }
                 /* se esiste mi fermo */
                 if (elementoEsiste) {
-                    return alert("errore");
+                    return alert("errore: contunuto già inserito");
                 } else {
                     /* altrimenti pusha nel array */
                     this.taskList.push(this.newTaskItem.trim());
                     this.newTaskItem = "";
                 }
             }
+
         }
     });
 });
